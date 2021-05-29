ecad6e81c6c2d619be34745d0de6e3c8cf9bf05e
unknown <jimpidas>
2021-05-29 23:34:40 +0530

    [Jyosmita]Added:Searching 63 in left and right tree recursively

--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ namespace BinarySearchTreeUsingGenerics
             binarySearch.Insert(63);
             binarySearch.Insert(67);
             binarySearch.Display();
+            bool result = binarySearch.IfExists(67, binarySearch);
+            Console.WriteLine(result);
         }
     }
 }
